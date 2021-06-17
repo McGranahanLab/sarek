92ca9b192a9304c300fb95eefdcf3dd985f40faf
Oriol Pich <opichros@gmail.com>
2021-06-17 13:00:56 +0100

    Update sender.sh

--- sender.sh
+++ sender.sh
@@ -2,6 +2,7 @@
 INPUT=$1
 DIRNAME=`dirname "$INPUT"`
 
-# should load Nextflow and Singularity prior to submission
+cd $DIRNAME
 
-nextflow run /SAN/colcc/TX100_WGS_sarek/src/sarek/main.nf --input $INPUT -profile singularity -c /SAN/colcc/TX100_WGS_sarek/src/sarek/ucl.conf –-work_dir $DIRNAME/working --outdir $DIRNAME/results  -resume --genome GRCh38 -qs 500 --igenomes_base /SAN/colcc/TX100_WGS_raw/wgs/data/references/ --tools HaplotypeCaller,Manta,Strelka,ASCAT,Mutect2 --skip_qc FastQC,bamQC,BCFtools
+# should load Nextflow and Singularity prior to submission
+nextflow run /SAN/colcc/TX100_WGS_sarek/src/sarek/main.nf --input $INPUT -profile singularity -c /SAN/colcc/TX100_WGS_sarek/src/sarek/ucl.conf -resume --genome GRCh38 -qs 500 --igenomes_base /SAN/colcc/TX100_WGS_raw/wgs/data/references/ --tools HaplotypeCaller,Manta,Strelka,ASCAT,Mutect2 --skip_qc FastQC,bamQC,BCFtools
